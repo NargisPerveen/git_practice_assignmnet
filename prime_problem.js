a02f084acc8feb6792a79b3be1949dbd4ca763e2
Nargis Perveen <nargisp360@gmail.com>
2022-10-20 00:23:04 +0530

    prime problem

--- prime_problem.js
+++ prime_problem.js
@@ -2,3 +2,14 @@ let num=13;
 for(let i=0;i<num;i++){
 if(num%i==1){
 console.log("prime number");
+
+// palindrome number
+
+let num=1221;
+let rev=0;
+let rem;
+while(num!=0){
+rem=num%10;
+rev=rev*10+rem;
+num=parseInt(num/num);
+
